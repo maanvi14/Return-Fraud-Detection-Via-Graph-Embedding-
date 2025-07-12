4435cf30b136606c5509f1a0b6ffd8a1d844c374
maanvi14 <maanvi.verma14@gmail.com>
2025-07-12 13:15:19 +0530

    Add missing success color shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,8 +26,10 @@ export default {
         success: {
           50: '#f0fdf4',
           100: '#dcfce7',
+          200: '#bbf7d0',
           500: '#22c55e',
           600: '#16a34a',
+          700: '#15803d',
         },
         warning: {
           50: '#fffbeb',
